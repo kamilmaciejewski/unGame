ca2c15f31aaf934d36004baf6c90164d88bf2297
Kamil Maciejewski <kamil.maciejewski@ericsson.com>
2018-07-16 10:10:26 +0200

    Bugfix

--- src/unGame/Creature.cpp
+++ src/unGame/Creature.cpp
@@ -15,7 +15,7 @@ Creature::~Creature() {
 void Creature::draw(SDL_Renderer* renderer, const int* screenWidht,
     const int* screenHeight) {
   if (texture == nullptr) {
-    texture = SDL_CreateTextureFromSurface(renderer, optimized_surface);
+    texture = SDL_CreateTextureFromSurface(renderer, surface);
     SDL_QueryTexture(texture, nullptr, nullptr, &rect_pos.w, &rect_pos.h);
   }
   if (isObjectOnScreen(screenWidht, screenHeight)) {
